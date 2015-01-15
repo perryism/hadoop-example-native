c712554af7892e9c4d1c483dabcdc153730cd7f0
agibsonccc <0@blix.io>
2015-01-15 12:40:45 -0800

    update conf

--- src/main/java/org/deeplearning4j/App.java
+++ src/main/java/org/deeplearning4j/App.java
@@ -12,7 +12,7 @@ import java.io.File;
  */
 public class App {
     public static void main( String[] args ) throws Exception {
-        File path = new ClassPathResource("ir.properties").getFile();
+        File path = new ClassPathResource("/conf/ir.properties").getFile();
         IRUnitDriver driver = new IRUnitDriver(path.getAbsolutePath());
         driver.setup();
         driver.simulateRun();
